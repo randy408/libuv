c662e4c977c335d2d6a4a3e4fa611d8263a4e224
Randy <randy408@protonmail.com>
2019-09-19 12:46:14 +0200

    add fuzz target

---
+++ test/uv_string_fuzzer.cc
@@ -0,0 +1,21 @@
+#include "../include/uv.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    size_t len = size + 1;
+    char *str = (char*)malloc(len);
+    str[size] = '\0';
+
+    struct sockaddr_in addr4;
+    struct sockaddr_in6 addr6;
+
+    int port = 12345;
+
+    uv_ip4_addr(str, port, &addr4);
+    uv_ip6_addr(str, port, &addr6);
+
+    return 0;
+}
